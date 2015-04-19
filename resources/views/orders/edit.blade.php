286b7c43b9f71568b95fde0008a9251c67822664
suecarmol <scarmol@mit.edu>
2015-04-19 02:49:07 -0500

    Added edit views

---
+++ resources/views/orders/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+	@include('includes.client_service.sidebar')
+	@section('content')
+		@include('orders.partials.form')
+		@if($errors->has())
+	    <div class="callout callout-danger">
+	    	<h4>Error</h4>
+
+	    	 @foreach ($errors->all('<p>:message</p>') as $message)
+            {!! $message !!}
+        	@endforeach
+	    </div>
+	  @endif
+	@stop
---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+	@include('includes.admin.sidebar')
+	@section('content')
+		@include('users.partials.form')
+		@if($errors->has())
+	    <div class="callout callout-danger">
+	    	<h4>Error</h4>
+
+	    	 @foreach ($errors->all('<p>:message</p>') as $message)
+            {!! $message !!}
+        	@endforeach
+	    </div>
+	  @endif
+	@stop
